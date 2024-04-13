6ba2e15bf5efea12d0120eb52c62946f76e72f7d
SangHeonKR <tkdgjs9768@naver.com>
2024-04-13 22:50:18 +0900

    수정커밋

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.swcapstone"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.swcapstone"
-        minSdk = 33
-        targetSdk = 33
+        minSdk = 34
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
