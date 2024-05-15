e39e9d496a24de7d824d59a723c6fb489d1b4205
SangHeonKR <tkdgjs9768@naver.com>
2024-05-16 00:41:34 +0900

    05_15_LSH5

--- app/src/main/java/com/example/swcapstone/FoodRecordFragment.kt
+++ app/src/main/java/com/example/swcapstone/FoodRecordFragment.kt
@@ -1,16 +1,19 @@
+// FoodRecordFragment.kt
+package com.example.swcapstone
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.swcapstone.R
+import androidx.fragment.app.Fragment
 
-class MainFragment : Fragment() {
+class FoodRecordFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.test, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food_record, container, false)
     }
 }
