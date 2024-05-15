9fae98c8917679a35a63ec5282753a5e150e7cbe
SangHeonKR <tkdgjs9768@naver.com>
2024-05-15 15:19:26 +0900

    05_15_LSH

--- app/src/main/java/com/example/swcapstone/RecordActivity.kt
+++ app/src/main/java/com/example/swcapstone/RecordActivity.kt
@@ -2,7 +2,6 @@ package com.example.swcapstone
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.swcapstone.R
 
 class RecordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
