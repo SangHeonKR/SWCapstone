91a4e3cbeabd172823e7f915dbca5c7b29d79965
SangHeonKR <tkdgjs9768@naver.com>
2024-05-21 18:38:56 +0900

    05_21_LSH_2

--- app/src/main/java/com/example/swcapstone/CommunityFragment.kt
+++ app/src/main/java/com/example/swcapstone/CommunityFragment.kt
@@ -16,17 +16,7 @@ class CommunityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // 레이아웃 인플레이트 및 뷰 초기화
-        val view = inflater.inflate(R.layout.activity_community, container, false)
-
-        // 네이버 카페 이미지 뷰 클릭 이벤트 처리
-        view.findViewById<ImageView>(R.id.naverCafeImageView).setOnClickListener {
-            openUrl("https://cafe.naver.com/pyurion/243666?art=ZXh0ZXJuYWwtc2VydmljZS1uYXZlci1zZWFyY2gtY2FmZS1wcg.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjYWZlVHlwZSI6IkNBRkVfVVJMIiwiY2FmZVVybCI6InB5dXJpb24iLCJhcnRpY2xlSWQiOjI0MzY2NiwiaXNzdWVkQXQiOjE3MTU5NzcxOTkzMDV9.TKh3JIE5FlyH5Xve3XVLVVbG1XCeg1KSJIcK7YaVtcg")
-        }
-
-        // 디스코드 이미지 뷰 클릭 이벤트 처리
-        view.findViewById<ImageView>(R.id.discordImageView).setOnClickListener {
-            openUrl("https://discord.gg/CWSKuxhV")
-        }
+        val view = inflater.inflate(R.layout.activity_blood, container, false)
 
         return view
     }
